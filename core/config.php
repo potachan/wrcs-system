8629711a5fc66cf7bf52ac7d77cee8192535e305
kenta <s153398@stn.nagaokaut.ac.jp>
2017-09-18 21:44:33 +0900

    make it better

--- core/config.php
+++ core/config.php
@@ -1,5 +1,12 @@
 <?php
-$host = "localhost";
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $dbname = substr($url["path"], 1);
+
+/*$host = "localhost";
 $username = "root";
 $password = "root";
-$dbname = "register_func";
+$dbname = "register_func";*/
