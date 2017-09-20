a50fa1a98ef14152c7b9df1aac5aeffb7539135b
kenta <s153398@stn.nagaokaut.ac.jp>
2017-09-20 22:47:46 +0900

    initial commit

--- portfolio.php
+++ portfolio.php
@@ -160,7 +160,7 @@ for ($i = 0; $i < 7; $i++) {
 </head>
 <body>
 <div class="container">
-<div class="col-xs-12 col-md-6 col-md-offset-3">
+<div class="col-xs-12 col-md-6">
 
 <h1>週間研究ポートフォリオ</h1>
 
