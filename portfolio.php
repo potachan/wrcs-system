851add6bb4b0e90c718c28eb55d264df772d319c
kenta <s153398@stn.nagaokaut.ac.jp>
2017-09-20 22:26:39 +0900

    initial commit

--- portfolio.php
+++ portfolio.php
@@ -173,7 +173,7 @@ for ($i = 0; $i < 7; $i++) {
 </tr>
 
 
-<tr><th class="col-md-1 col-xs-3 col-md-offset-3">日付</th><th class="col-md-1 col-xs-4">時間</th><th class="col-md-8 col-xs-5">内容</th></tr>
+<tr><th class="col-md-1 col-xs-3">日付</th><th class="col-md-1 col-xs-4">時間</th><th class="col-md-4 col-xs-5">内容</th></tr>
 <?php echo "$table"; ?>
 
 
