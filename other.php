dd6ce86c76a3fdfe2bf1374fcc543be599139462
kenta <s153398@stn.nagaokaut.ac.jp>
2017-09-24 22:55:18 +0900

    make it better

--- other.php
+++ other.php
@@ -59,7 +59,7 @@ $result->close();
   while ($row = $result->fetch_assoc()) {
       $username = $row['username'];
       $student_id = $row['student_id'];
-      echo "<tr><td>$student_id</td><td><a href="home.php?">$username</a></td></tr>";
+      echo "<tr><td>$student_id</td><td><a href='home.php?'>$username</a></td></tr>";
   }
 ?>
 
