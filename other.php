d4a151910b952ce4ce46b0e38c90e10c36efc4a0
kenta <s153398@stn.nagaokaut.ac.jp>
2017-09-24 22:38:43 +0900

    make it better

--- other.php
+++ other.php
@@ -7,6 +7,8 @@ if(!isset($_SESSION['user'])) {
 	header("Location: index.php");
 }
 
+include_once 'dbconnect.php';
+
 // ユーザーIDからユーザー名を取り出す
 $query = "SELECT * FROM users WHERE user_id=".$_SESSION['user']."";
 $result = $mysqli->query($query);
@@ -27,39 +29,52 @@ while ($row = $result->fetch_assoc()) {
 // データベースの切断
 $result->close();
 
-?>
+?> 
+
 <!DOCTYPE HTML>
 <html lang="ja">
 <head>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>週報作成システム</title>
-<link rel="stylesheet" href="style.css">
+<title>週報作成支援システム</title>
+<link rel="stylesheet" type="text/css" href="style.css">
 <!-- Bootstrap読み込み（スタイリングのため） -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 </head>
 <body>
+
 <div class="col-xs-12 col-md-6 col-md-offset-3">
 
-<h2 class="visible-xs" align="center">プロフィール</h2>
-<h1 class="hidden-xs">プロフィール</h1>
-<ul>
-	<li>名前：<?php echo $username; ?></li>
-</ul>
+<h2 class="visible-xs" align="center">週間研究ポートフォリオ</h2>
+<h1 class="hidden-xs">週間研究ポートフォリオ</h1>
+
+<table class="table table-striped table-bordered table-hover table-condensed">
+
+<tr><th class="col-md-1 col-xs-3">氏名</th><th class="col-md-2 col-xs-4">学籍番号</th></tr>
+
+<?php
+  $query = "SELECT * FROM users";
+  $result = $mysqli->query($query);
+  while ($row = $result->fetch_assoc()) {
+      $username = $row['username'];
+      $student_id = $row['student_id'];
+      echo "<tr><td> $username</td><td>$student_id　</td></tr>";
+  }
+?>
+
+</table>
 
  <ul class="nav nav-tabs nav-justified">
-    <li class="active"><a href="#">ホーム</a></li>
+    <li><a href="home.php?record">ホーム</a></li>
 	<li><a href="record.php?record">記録ページ</a></li>
 	<li><a href="portfolio.php?portfolio">ポートフォリオ</a></li>
-	<li><a href="other.php?">他者のポートフォリオ</a></li>
+	<li class="active"><a href="#">他者のポートフォリオ</a></li>
 	<li><a href="logout.php?logout">ログアウト</a></li>
  </ul>
-
-
-
 
 
 </div>
 </body>
 </html>
+
