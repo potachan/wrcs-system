867c75a21bd7abff9edf1bc9dffc2c0f27603d1b
kenta <s153398@stn.nagaokaut.ac.jp>
2017-11-27 01:32:20 +0900

    make it better

--- register.php
+++ register.php
@@ -58,10 +58,7 @@ if(isset($_POST['signup'])) {
 		<input type="password" class="form-control" name="password" placeholder="パスワード" required />
 	</div>
 	<button type="submit" class="btn btn-default" name="signup">会員登録する</button>
-	<br>　
-	たまご
-	<br>
-	<a href="index.php">ログインはこちら</a>
+	<button type="submit" class="btn btn-link" name="signup">会員登録する</button>
 </form>
 
 </div>
